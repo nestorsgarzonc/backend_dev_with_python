c9599347e6f09732aa55b785ab269b3afba60f7d
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-04-30 18:41:57 -0500

    Added custom decoratos

--- fastapi_version/app.py
+++ fastapi_version/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from decorators.font_styles import make_bold, make_emphasis, make_underlined
+
 app = FastAPI()
 
 
@@ -12,3 +14,11 @@ def index():
 def bye(name: str):
     data = f'<h1>Bye {name}! :)</h1>'
     return data
+
+
+@app.get('/bye', response_class=HTMLResponse)
+@make_bold
+@make_emphasis
+@make_underlined
+def bye_with_decorators():
+    return 'Bye'
--- flask_version/app.py
+++ flask_version/app.py
@@ -1,3 +1,4 @@
+from decorators.font_styles import make_bold, make_emphasis, make_underlined
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,6 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'HW'
+
+
+@app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underlined
+def bye_with_decorators():
+    return 'Bye'
 
 
 @app.route('/bye/<name>')
