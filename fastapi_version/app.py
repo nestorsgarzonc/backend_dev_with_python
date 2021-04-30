811464bf73cc6470af5ce0e1ae7adb30f2092873
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-04-30 18:06:10 -0500

    Using query params

--- fastapi_version/app.py
+++ fastapi_version/app.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get('/')
 def index():
     return 'HW'
+
+
+@app.get('/bye/{name}')
+def bye(name: str):
+    return f'Bye {name}! :)'
--- flask_version/app.py
+++ flask_version/app.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'HW'
+
+
+@app.route('/bye/<name>')
+def bye(name):
+    return f'Bye {name}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
