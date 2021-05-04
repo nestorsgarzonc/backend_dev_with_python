146083566f80e8e8a1c3a7ce479ba9e1180f068c
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-05-04 14:17:51 -0500

    Added age and gender api

--- using_templates/fast_api/app.py
+++ using_templates/fast_api/app.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from random import randint
 from datetime import datetime
-
+import requests
 
 app = FastAPI()
 
@@ -22,5 +22,22 @@ def home(request: Request):
             'title': 'NW by FastApi',
             'number': randint(0, 100),
             'year': datetime.now().year,
+        }
+    )
+
+
+@app.get('/guess/{name}')
+async def guess(request: Request, name: str):
+    res_y = requests.get('https://api.agify.io', params={'name': name})
+    res_g = requests.get('https://api.genderize.io', params={'name': name})
+    gender = res_g.json()['gender']
+    years = res_y.json()['age']
+    return templates.TemplateResponse(
+        'guess.html',
+        {
+            'request': request,
+            'title': name,
+            'gender': gender,
+            'year': years,
         }
     )
--- using_templates/flask/app.py
+++ using_templates/flask/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from random import randint
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -12,6 +13,20 @@ def home():
         title='NW by Flask',
         number=randint(0, 100),
         year=datetime.today().year
+    )
+
+
+@app.route('/guess/<name>')
+def guess(name: str):
+    res_y = requests.get('https://api.agify.io', params={'name': name})
+    res_g = requests.get('https://api.genderize.io', params={'name': name})
+    gender = res_g.json()['gender']
+    years = res_y.json()['age']
+    return render_template(
+        'guess.html',
+        title=name,
+        gender=gender,
+        year=years,
     )
 
 
