2c4376026340a6d20e7802fecb5592b4c1ff4022
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-05-13 12:37:28 -0500

    Added flask form

---
+++ forms/flask/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, redirect
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Email, Length
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+Bootstrap(app)
+
+app.secret_key = "some secret string"
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='password', validators=[
+                             DataRequired(),
+                             Length(min=8),
+                             ])
+    submit = SubmitField(label='submit')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    form.validate_on_submit()
+
+    return render_template('login.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
